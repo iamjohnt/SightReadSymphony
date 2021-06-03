5beb4b6a3647a7d5a9b8ee515859a5d7de499769
johnt <tranjohn123@gmail.com>
2021-06-03 16:14:46 -0400

    expanded unit tests for NoteRangeOptions

--- src/test/java/NoteRangeOptionsTest.java
+++ src/test/java/NoteRangeOptionsTest.java
@@ -1,90 +1,342 @@
+import exceptions.InvalidOptionsException;
 import gameoptions.NoteRangeOptions;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/* this test is divided into 4 nested tests
+* test setMinBass()
+* test setMaxBass()
+* test setMinTreble()
+* test setMaxTreble()
+* I recommend using IDE to view method structure, or fold methods, for convenience
+* */
+@DisplayName("NoteRangeOptions test")
 public class NoteRangeOptionsTest {
 
     private NoteRangeOptions range;
 
-    @BeforeEach public void init_all_valid_data() {
+    @BeforeEach
+    public void init_all_valid_data() {
         range = new NoteRangeOptions();
-        range.setMinBass(30);
-        range.setMaxBass(60);
-        range.setMinTreble(70);
-        range.setMaxTreble(100);
     }
 
     @AfterEach public void teardown() {
         range = null;
     }
 
-    @Test
-    public void min_notes_too_low() {
-        range.setMinBass(0);
-        assertFalse(range.isValid());
-
-        range.setMinTreble(0);
-        assertFalse(range.isValid());
+    @Nested
+    @DisplayName("Test isValid()")
+    class testIsValid {
+        @Test
+        public void is_exists_null_min_or_max() {
+            try {
+                range.setMinBass(30);
+                // purposefully missing maxBass
+                range.setMinTreble(50);
+                range.setMaxTreble(70);
+            } catch (Exception e) {
+                assertTrue(e instanceof InvalidOptionsException);
+            }
+        }
     }
 
-    @Test
-    public void max_notes_too_high() {
-        range.setMaxBass(120);
-        assertFalse(range.isValid());
+    @Nested
+    @DisplayName("Test setMinBass()")
+    class testSetMinBass {
 
-        range.setMaxTreble(120);
-        assertFalse(range.isValid());
+        @Test
+        public void minBass_slightly_below_thresh() {
+            Exception exception = null;
+            try {
+                range.setMinBass(20);
+            } catch (Exception e) {
+                exception = e;
+            }
+            assertTrue(exception != null);
+        }
+
+        @Test
+        public void minBass_at_lower_thresh() {
+            try {
+                range.setMinBass(21);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            assertTrue(range.getMinBass() == 21);
+        }
+
+        @Test
+        public void minBass_at_upper_bound() {
+            try {
+                range.setMinBass(108);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            assertTrue(range.getMinBass() == 108);
+        }
+
+        @Test
+        public void minBass_slightly_above_upper_bound() {
+            Exception exception = null;
+            try {
+                range.setMinBass(109);
+            } catch (Exception e) {
+                exception = e;
+            }
+            assertTrue(exception != null);
+        }
+
+        @Test
+        public void minBass_crosses_maxBass() {
+            String msg = null;
+            try {
+                // the order of this matters, since the first setter may throw exception first, and skip execution of 2nd setter
+                range.setMaxBass(49);
+                range.setMinBass(50);
+            } catch (Exception e) {
+                msg = e.getMessage();
+            }
+            assertTrue(msg.contains(InvalidOptionsException.MSG_MIN_OVER_MAX));
+        }
+
+        @Test
+        public void minBass_equals_maxBass() {
+            String msg = null;
+            try {
+                range.setMinBass(50);
+                range.setMaxBass(50);
+            } catch (Exception e) {
+                msg = e.getMessage();
+            }
+            assertTrue(msg == null);
+            assertTrue(msg == null);
+            assertTrue(range.getMinBass().equals(50));
+            assertTrue(range.getMinBass().equals(50));
+        }
     }
 
-    @Test
-    public void min_note_near_and_at_low_threshold() {
-        range.setMinBass(20);           // 1 below thresh
-        assertFalse(range.isValid());
-        range.setMinBass(21);           // just at thresh to be valid
-        assertTrue(range.isValid());
-        range.setMinBass(22);           // 1 above thresh
-        assertTrue(range.isValid());
+    @Nested
+    @DisplayName("Test setMaxBass")
+    class testingMaxBassOutOfBounds {
 
-        range.setMinTreble(20);           // 1 below thresh
-        assertFalse(range.isValid());
-        range.setMinTreble(21);           // just at thresh to be valid
-        assertTrue(range.isValid());
-        range.setMinTreble(22);           // 1 above thresh
-        assertTrue(range.isValid());
+        @Test
+        public void maxBass_slightly_below_thresh() {
+            String msg = null;
+            try {
+                range.setMaxBass(20);
+            } catch (Exception e) {
+                msg = e.getMessage();
+            }
+            assertTrue(msg.contains(InvalidOptionsException.MSG_OUT_OF_BOUNDS));
+        }
+
+        @Test
+        public void maxBass_at_lower_thresh() {
+            try {
+                range.setMaxBass(21);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            assertTrue(range.getMaxBass() == 21);
+        }
+
+        @Test
+        public void maxBass_at_upper_bound() {
+            try {
+                range.setMaxBass(108);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            assertTrue(range.getMaxBass() == 108);
+        }
+
+        @Test
+        public void maxBass_slightly_above_upper_bound() {
+            String msg = null;
+            try {
+                range.setMaxBass(109);
+            } catch (Exception e) {
+                msg = e.getMessage();
+            }
+            assertTrue(msg.contains(InvalidOptionsException.MSG_OUT_OF_BOUNDS));
+        }
+
+        @Test
+        public void maxBass_crosses_maxBass() {
+            String msg = null;
+            try {
+                // the order of this matters, since the first setter may throw exception first, and skip execution of 2nd setter
+                range.setMinBass(50);
+                range.setMaxBass(49);
+            } catch (Exception e) {
+                msg = e.getMessage();
+            }
+            assertTrue(msg.contains(InvalidOptionsException.MSG_MIN_OVER_MAX));
+        }
+
+        @Test
+        public void maxBass_equals_maxBass() {
+            String msg = null;
+            try {
+                range.setMinBass(50);
+                range.setMaxBass(50);
+            } catch (Exception e) {
+                msg = e.getMessage();
+            }
+            assertTrue(msg == null);
+            assertTrue(range.getMinBass().equals(50));
+            assertTrue(range.getMinBass().equals(50));
+        }
     }
 
-    @Test
-    public void max_near_and_at_high_threshold() {
-        range.setMinBass(107);
-        assertTrue(range.isValid());
-        range.setMinBass(108);
-        assertTrue(range.isValid());
-        range.setMinBass(109);
-        assertFalse(range.isValid());
+    @Nested
+    @DisplayName("Test setMinTreble")
+    class testSetMinTreble {
 
-        range.setMinTreble(107);
-        assertTrue(range.isValid());
-        range.setMinTreble(108);
-        assertTrue(range.isValid());
-        range.setMinTreble(109);
-        assertFalse(range.isValid());
+        @Test
+        public void minTreble_slightly_below_thresh() {
+            String msg = null;
+            try {
+                range.setMinTreble(20);
+            } catch (Exception e) {
+                msg = e.getMessage();
+            }
+            assertTrue(msg.contains(InvalidOptionsException.MSG_OUT_OF_BOUNDS));
+        }
+
+        @Test
+        public void minTreble_at_lower_thresh() {
+            try {
+                range.setMinTreble(21);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            assertTrue(range.getMinTreble() == 21);
+        }
+
+        @Test
+        public void minTreble_at_upper_bound() {
+            try {
+                range.setMinTreble(108);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            assertTrue(range.getMinTreble() == 108);
+        }
+
+        @Test
+        public void minTreble_slightly_above_upper_bound() {
+            String msg = null;
+            try {
+                range.setMinTreble(109);
+            } catch (Exception e) {
+                msg = e.getMessage();
+            }
+            assertTrue(msg.contains(InvalidOptionsException.MSG_OUT_OF_BOUNDS));
+        }
+
+        @Test
+        public void minTreble_crosses_maxTreble() {
+            String msg = null;
+            try {
+                // the order of this matters, since the first setter may throw exception first, and skip execution of 2nd setter
+                range.setMaxTreble(49);
+                range.setMinTreble(50);
+            } catch (Exception e) {
+                msg = e.getMessage();
+            }
+            assertTrue(msg.contains(InvalidOptionsException.MSG_MIN_OVER_MAX));
+        }
+
+        @Test
+        public void maxBass_equals_maxBass() {
+            String msg = null;
+            try {
+                range.setMinBass(50);
+                range.setMaxBass(50);
+            } catch (Exception e) {
+                msg = e.getMessage();
+            }
+            assertTrue(msg == null);
+            assertTrue(msg == null);
+            assertTrue(range.getMinTreble().equals(50));
+            assertTrue(range.getMinTreble().equals(50));
+        }
     }
 
+    @Nested
+    @DisplayName("Test setMaxTreble")
+    class testSetMaxTreble {
+        @Test
+        public void maxTreble_slightly_below_thresh() {
+            String msg = null;
+            try {
+                range.setMaxTreble(20);
+            } catch (Exception e) {
+                msg = e.getMessage();
+            }
+            assertTrue(msg.contains(InvalidOptionsException.MSG_OUT_OF_BOUNDS));
+        }
 
-    @Test
-    public void min_bass_higher_than_max_bass() {
-        range.setMinBass(70);
-        range.setMaxBass(69);
-        assertFalse(range.isValid());
+        @Test
+        public void maxTreble_at_lower_thresh() {
+            try {
+                range.setMaxTreble(21);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            assertTrue(range.getMaxTreble() == 21);
+        }
+
+        @Test
+        public void maxTreble_at_upper_bound() {
+            try {
+                range.setMaxTreble(108);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            assertTrue(range.getMaxTreble() == 108);
+        }
+
+        @Test
+        public void maxTreble_slightly_above_upper_bound() {
+            Exception exception = null;
+            try {
+                range.setMaxTreble(109);
+            } catch (Exception e) {
+                exception = e;
+            }
+            assertTrue(exception.getMessage().contains(InvalidOptionsException.MSG_OUT_OF_BOUNDS));
+        }
+
+        @Test
+        public void minTreble_crosses_maxTreble() {
+            String msg = null;
+            try {
+                // the order of this matters, since the first setter may throw exception first, and skip execution of 2nd setter
+                range.setMinTreble(50);
+                range.setMaxTreble(49);
+            } catch (Exception e) {
+                msg = e.getMessage();
+            }
+            assertTrue(msg.contains(InvalidOptionsException.MSG_MIN_OVER_MAX));
+        }
+
+        @Test
+        public void maxBass_equals_maxBass() {
+            String msg = null;
+            try {
+                range.setMinBass(50);
+                range.setMaxBass(50);
+            } catch (Exception e) {
+                msg = e.getMessage();
+            }
+            assertTrue(msg == null);
+            assertTrue(range.getMinTreble().equals(50));
+            assertTrue(range.getMinTreble().equals(50));
+        }
+
     }
 
-    @Test
-    public void min_treble_higher_than_max_treble() {
-        range.setMinTreble(70);
-        range.setMaxTreble(69);
-        assertFalse(range.isValid());
-    }
 }
