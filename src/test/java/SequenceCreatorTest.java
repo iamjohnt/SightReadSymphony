41f7218d4f9dcad3c158b23c119805ea920e04aa
johnt <tranjohn123@gmail.com>
2021-06-25 22:12:56 -0400

    created basic SequenceCreator, which manages midi sequence

--- src/test/java/SequenceCreatorTest.java
+++ src/test/java/SequenceCreatorTest.java
@@ -14,7 +14,11 @@ public class SequenceCreatorTest {
 
     @BeforeEach
     public void init() {
-        sc = new SequenceCreator();
+        try {
+            sc = new SequenceCreator();
+        } catch (InvalidMidiDataException e) {
+            e.printStackTrace();
+        }
     }
 
     @AfterEach
@@ -23,16 +27,56 @@ public class SequenceCreatorTest {
     }
 
     @Test
-    public void rand_note_within_bounds() {
-        SequenceCreator sc = new SequenceCreator();
-        MidiEvent event = null;
+    public void set_and_repeat_note_from_sequence() {
         try {
-            event = sc.createRandMidiEvent(21, 108);
+            sc.addToTreble(5);
+            assertEquals(sc.getNoteFromTreble(0), 5);
         } catch (InvalidMidiDataException e) {
             e.printStackTrace();
         }
-        ShortMessage sm = (ShortMessage) event.getMessage();
-        int resultNote = sm.getData1();
-        assertTrue(resultNote >= 21 && resultNote <= 108);
+    }
+
+    @Test
+    public void populate_and_confirm_sequence() {
+        for (int i = 0; i < 10; i++) {
+            try {
+                sc.addToTreble(i);
+            } catch (InvalidMidiDataException e) {
+                e.printStackTrace();
+            }
+        }
+        for (int i = 0; i < 10; i++) {
+            assertEquals(sc.getNoteFromTreble(i), i);
+        }
+    }
+
+    @Test
+    public void populate_rand_and_confirm_everything_populated_fine() {
+        try {
+            sc.populateTrebleRandMidiEvents(20,21, 108);
+            sc.populateBassRandMidiEvents(20,21,108);
+        } catch (InvalidMidiDataException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < 20; i++) {
+            assertEquals(sc.getTrebleSize(), 20);
+            assertEquals(sc.getBassSize(), 20);
+        }
+    }
+
+    @Test
+    public void populate_rand_and_print_it_to_see_if_actually_random() {
+        try {
+            sc.populateTrebleRandMidiEvents(20,21, 108);
+            sc.populateBassRandMidiEvents(20,21,108);
+        } catch (InvalidMidiDataException e) {
+            e.printStackTrace();
+        }
+
+        int treblePrev = 0;
+        int bassPrev = 0;
+        for (int i = 0; i < 20; i++) {
+            System.out.println(sc.getNoteFromTreble(i) + " " + sc.getNoteFromBass(i));;
+        }
     }
 }
