50c5d12836ccffa8ec6069db6de9de024552a608
johnt <tranjohn123@gmail.com>
2021-06-28 16:00:36 -0400

    updated TrackParser to consider not all MidiEvents in Track will contain ShortMessages

--- src/main/java/midi/TrackParser.java
+++ src/main/java/midi/TrackParser.java
@@ -1,6 +1,7 @@
 package midi;
 
 import javax.sound.midi.*;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Spliterator;
 import java.util.function.Consumer;
@@ -8,13 +9,22 @@ import java.util.function.Consumer;
 public class TrackParser {
 
     public int[] convertTrackToMidiIntegerArray(Track track) {
-        int[] rtn = new int[track.size()];
-        int i = 0;
-        while (track.get(i) != null) {
-            MidiEvent event = track.get(i);
-            if (event.getMessage() instanceof ShortMessage) {
-                ShortMessage msg = (ShortMessage) event.getMessage();
-                rtn[i] = msg.getData1();
+        int rtn[] = new int[countShortMessages(track)];
+        for (int i = 0; i < track.size(); i++){
+            ShortMessage sm = null;
+            if (track.get(i).getMessage() instanceof ShortMessage) {
+                sm = (ShortMessage) track.get(i).getMessage();
+                rtn[i] = (sm.getData1());
+            }
+        }
+        return rtn;
+    }
+
+    private int countShortMessages(Track track) {
+        int rtn = 0;
+        for (int i = 0; i < track.size(); i++) {
+            if (track.get(i).getMessage() instanceof ShortMessage) {
+                rtn++;
             }
         }
         return rtn;
