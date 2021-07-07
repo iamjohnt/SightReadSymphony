e51769d04fcba53c8ea2fa50c9049b21000683e2
johnt <tranjohn123@gmail.com>
2021-07-07 16:30:45 -0400

    extracted calculating note, accidental, and octave into their own private methods

--- src/main/java/draw/MusicNote.java
+++ src/main/java/draw/MusicNote.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 
 public class MusicNote {
 
+    // these public final static fields, make it easier to know what to pass into this obj's constructor
     public final static int A = 0;
     public final static int B = 1;
     public final static int C = 2;
@@ -11,25 +12,18 @@ public class MusicNote {
     public final static int E = 4;
     public final static int F = 5;
     public final static int G = 6;
-    public final static int ZERO = 0;
-    public final static int ONE = 1;
-    public final static int TWO = 2;
-    public final static int THREE = 3;
-    public final static int FOUR = 4;
-    public final static int FIVE = 5;
-    public final static int SIX = 6;
-    public final static int SEVEN = 7;
-    public final static int EIGHT = 8;
     public final static int FLAT = -1;
     public final static int NO_ACCIDENTAL = 0;
     public final static int SHARP = 1;
 
+    // instance variables
     private int midiVal;
     private int note;
     private int octave;
     private int accidental;
 
     public MusicNote(int note, int octave, int accidental) {
+        // todo calculate midi value based on note, octave, and accidental
         this.note = note;
         this.octave = octave;
         this.accidental = accidental;
@@ -43,42 +37,54 @@ public class MusicNote {
             temp = temp - 12;
             iterationCount++;
         }
+        this.note = calcNoteLetter(midiVal, accidental);
+        this.octave = calcOctave(midiVal, accidental);
+        this.accidental = calcAccidental(midiVal, accidental);
+    }
 
-        // determine octave
+    private int calcOctave(int midiVal, int accidental) {
+        int iterationCount = 0;
+        int oct;
+        int temp = midiVal;
+        while (temp > 32) {
+            temp -= 12;
+            iterationCount++;
+        }
         if (temp < 23) {
-            this.octave = iterationCount;
+            oct = iterationCount;
         } else {
-            this.octave = iterationCount + 1;
+            oct = iterationCount + 1;
         }
+        return oct;
+    }
 
-        // determine notes that never have accidental
-        boolean isWhiteKey = temp == 21 || temp == 23 || temp == 24 || temp == 26 || temp == 28 || temp == 29 || temp == 31;
-
-        // determine note
+    private int calcNoteLetter(int midiVal) {
+        int temp = midiVal;
+        while (temp > 32) {
+            temp -= 12;
+        }
         int[] flatPattern = {A, A, B, C, C, D, D, E, F, F, G, G};
-        int tempNote = flatPattern[temp - 21];
-        if (accidental == FLAT && !isWhiteKey) {
-            tempNote++;
+        int noteLetter = flatPattern[temp - 21];
+        if (accidental == FLAT && !isWhiteKey(midiVal)) { // TODO fix this, it needs to determine the note letter based on not just FLAT, but SHARP and NONE as well
+            noteLetter++;
         }
-        this.note = tempNote;
+        return noteLetter;
+    }
 
-        // determine accidental
-        if (isWhiteKey) {
-            this.accidental = 0;
+    private int calcAccidental(int midiVal, int accidental) {
+        if (isWhiteKey(midiVal)) {
+            return MusicNote.NO_ACCIDENTAL;
         } else {
-            this.accidental = accidental;
+            return accidental;
         }
+    }
 
-        // number to note string, and accidental to string
-        char[] noteStr = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
-        String accStr = "";
-        if (this.accidental == -1) {
-            accStr = "FLAT";
-        } else if (this.accidental == 1) {
-            accStr = "SHARP";
-        } else if (this.accidental == 0) {
-            accStr = "";
+    private boolean isWhiteKey(int midiVal) {
+        int temp = midiVal;
+        while (temp > 32) {
+            temp -= 12;
         }
+        return temp == 21 || temp == 23 || temp == 24 || temp == 26 || temp == 28 || temp == 29 || temp == 31;
     }
 
     public int getMidiVal() {
