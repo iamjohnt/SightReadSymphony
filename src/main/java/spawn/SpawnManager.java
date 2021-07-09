c2cc39257ba75df3df33a465ed5e2913847e0b76
johnt <tranjohn123@gmail.com>
2021-07-09 11:21:47 -0400

    separated filling rectangle from creation, since that caused a bug saying internal graphics havent been initiated yet

--- src/main/java/spawn/SpawnManager.java
+++ src/main/java/spawn/SpawnManager.java
@@ -3,9 +3,11 @@ package spawn;
 import draw.CoordManager;
 import draw.MusicNote;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -30,32 +32,40 @@ public class SpawnManager {
     public static final int TAIL_NONE = 0;
     public static final int TAIL_UP = 1;
     public static final int TAIL_DOWN = -1;
-    public static final String PATH_WHOLE_NOTE = "";
-//    public static final String PATH_HALF_NOTE_TAIL_UP = "";
-//    public static final String PATH_HALF_NOTE_TAIL_DOWN = "";
-//    public static final String PATH_QUARTER_NOTE_TAIL_UP = "";
-//    public static final String PATH_QUARTER_NOTE_TAIL_DOWN = "";
+    public static final String PATH_WHOLE_NOTE = "src/main/resources/images/single_note.png";
     private double lineHeight = 18;
     private double distTailFromNote = 36; // for now
 
-    public Rectangle createTrebleNote(MusicNote note, int durationType, int tailOrientation, int x) {
-        CoordManager coord = new CoordManager();
+    public Rectangle createRectangleToHoldNote(MusicNote note, int durationType, int tailOrientation, int x) {
+        CoordManager coord = new CoordManager(0,0,lineHeight);
         double width = calcWidth(lineHeight);
         double height = calcHeight(tailOrientation);
         double y = calcNewY(note, tailOrientation);
         Rectangle rect = new Rectangle(x,y,width,height);
         String urlString = null;
         try {
-            urlString = new URL(PATH_WHOLE_NOTE).toURI().toURL().toString();
-        } catch (MalformedURLException | URISyntaxException e) {
+            urlString = new File(PATH_WHOLE_NOTE).toURI().toURL().toString();
+        } catch (MalformedURLException e) {
             e.printStackTrace();
         }
-        rect.setFill(new ImagePattern(new Image(urlString)));
         return rect;
     }
 
+    public void fillRectangleWithNote(Rectangle rect, int durationType) {
+        String urlString = null;
+        switch (durationType) {
+            case WHOLE:
+                try {
+                    urlString = new File(PATH_WHOLE_NOTE).toURI().toURL().toString();
+                    rect.setFill(new ImagePattern(new Image(urlString)));
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+        }
+    }
+
     private double calcNewY(MusicNote note, int tailOrientation) {
-        CoordManager coord = new CoordManager();
+        CoordManager coord = new CoordManager(0,0, lineHeight);
         switch (tailOrientation) {
             case TAIL_NONE: return coord.getTrebleYCoord(note);
             case TAIL_DOWN: return coord.getTrebleYCoord(note);
