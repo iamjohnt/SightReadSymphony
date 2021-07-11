4d7e1bdeac53da7bed884a003e4a0ee2b28eff80
johnt <tranjohn123@gmail.com>
2021-07-11 11:29:07 -0400

    created unit tests for Note constructors

--- src/test/java/test/note_coord/NoteTest.java
+++ src/test/java/test/note_coord/NoteTest.java
@@ -12,7 +12,7 @@ public class NoteTest {
 
     @Nested
     @DisplayName("test constructors")
-    private class Constructors {
+    public class Constructors {
         /* psuedo scratchpad please ignore
          * what do i need to test
          * run
@@ -31,8 +31,36 @@ public class NoteTest {
         }
 
         @Test
-        public void test_construct_using_midi_value_and_requested_accidental() {
-            Note A0 = new Note(StaticValuesMidi.A_0, Note.NO_ACCIDENTAL);
+        public void construct_with_id() {
+            Note c4 = new Note(StaticValuesNamedNote.C_4);
+            int C = Note.C;
+            int NONE = Note.NO_ACCIDENTAL;
+            int FOUR = Note.FOUR;
+            assertEquals(C, c4.getNoteLetter());
+            assertEquals(NONE, c4.getAccidental());
+            assertEquals(FOUR, c4.getOctave());
+        }
+
+        @Test
+        public void construct_with_midiValue_and_requestedAccidental() {
+            Note a0 = new Note(StaticValuesMidi.A_0, Note.NO_ACCIDENTAL);
+            int A = Note.A;
+            int NONE = Note.NO_ACCIDENTAL;
+            int ZERO = Note.ZERO;
+            assertEquals(A, a0.getNoteLetter());
+            assertEquals(NONE, a0.getAccidental());
+            assertEquals(ZERO, a0.getOctave());
+        }
+
+        @Test
+        public void construct_with_noteLetter_accidental_octave() {
+            Note c8 = new Note(Note.C, Note.NO_ACCIDENTAL, Note.EIGHT);
+            int C = 2;
+            int NONE = 1;
+            int EIGHT = 8;
+            assertEquals(C, c8.getNoteLetter());
+            assertEquals(NONE, c8.getAccidental());
+            assertEquals(EIGHT, c8.getOctave());
         }
     }
 
