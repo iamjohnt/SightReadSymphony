e769ec100190aca6254e8d90b672b54fd8729459
johnt <tranjohn123@gmail.com>
2021-07-12 19:23:00 -0400

    created and implemented MidiNote, NamedNote, and a class that maps them to each other

---
+++ src/main/java/note_coordinate/MidiAndNoteBimap.java
@@ -0,0 +1,307 @@
+package note_coordinate;
+
+import java.util.HashMap;
+
+public class MidiAndNoteBimap {
+
+    private int[] midiIDArray;
+    private int[] noteIDArray;
+    private HashMap<Integer, Integer> midiToNote = new HashMap<>();
+    private HashMap<Integer, Integer> noteToMidi = new HashMap<>();
+
+    public MidiAndNoteBimap() {
+        initMidiArray();
+        initNoteArray();
+        mapBothWays();
+    }
+
+    public int getNoteIDWithMidiID(int midiID) {
+        return midiToNote.get(midiID);
+    }
+
+    public int getMidiIDWithNoteID(int noteID) {
+        return noteToMidi.get(noteID);
+    }
+
+    private void mapBothWays() {
+        for (int i = 0; i < midiIDArray.length; i++) {
+            midiToNote.put(midiIDArray[i], noteIDArray[i]);
+            noteToMidi.put(noteIDArray[i], midiIDArray[i]);
+        }
+    }
+
+    public void initMidiArray() {
+        midiIDArray = new int[]{
+            MidiNote.A_0,
+            MidiNote.A_SHARP_0,
+            MidiNote.B_FLAT_0,
+            MidiNote.B_0,
+    
+            MidiNote.C_1,
+            MidiNote.C_SHARP_1,
+            MidiNote.D_FLAT_1,
+            MidiNote.D_1,
+            MidiNote.D_SHARP_1,
+            MidiNote.E_FLAT_1,
+            MidiNote.E_1,
+            MidiNote.F_1,
+            MidiNote.F_SHARP_1,
+            MidiNote.G_FLAT_1,
+            MidiNote.G_1,
+            MidiNote.G_SHARP_1,
+            MidiNote.A_FLAT_1,
+            MidiNote.A_1,
+            MidiNote.A_SHARP_1,
+            MidiNote.B_FLAT_1,
+            MidiNote.B_1,
+    
+            MidiNote.C_2,
+            MidiNote.C_SHARP_2,
+            MidiNote.D_FLAT_2,
+            MidiNote.D_2,
+            MidiNote.D_SHARP_2,
+            MidiNote.E_FLAT_2,
+            MidiNote.E_2,
+            MidiNote.F_2,
+            MidiNote.F_SHARP_2,
+            MidiNote.G_FLAT_2,
+            MidiNote.G_2,
+            MidiNote.G_SHARP_2,
+            MidiNote.A_FLAT_2,
+            MidiNote.A_2,
+            MidiNote.A_SHARP_2,
+            MidiNote.B_FLAT_2,
+            MidiNote.B_2,
+    
+            MidiNote.C_3,
+            MidiNote.C_SHARP_3,
+            MidiNote.D_FLAT_3,
+            MidiNote.D_3,
+            MidiNote.D_SHARP_3,
+            MidiNote.E_FLAT_3,
+            MidiNote.E_3,
+            MidiNote.F_3,
+            MidiNote.F_SHARP_3,
+            MidiNote.G_FLAT_3,
+            MidiNote.G_3,
+            MidiNote.G_SHARP_3,
+            MidiNote.A_FLAT_3,
+            MidiNote.A_3,
+            MidiNote.A_SHARP_3,
+            MidiNote.B_FLAT_3,
+            MidiNote.B_3,
+    
+            MidiNote.C_4,
+            MidiNote.C_SHARP_4,
+            MidiNote.D_FLAT_4,
+            MidiNote.D_4,
+            MidiNote.D_SHARP_4,
+            MidiNote.E_FLAT_4,
+            MidiNote.E_4,
+            MidiNote.F_4,
+            MidiNote.F_SHARP_4,
+            MidiNote.G_FLAT_4,
+            MidiNote.G_4,
+            MidiNote.G_SHARP_4,
+            MidiNote.A_FLAT_4,
+            MidiNote.A_4,
+            MidiNote.A_SHARP_4,
+            MidiNote.B_FLAT_4,
+            MidiNote.B_4,
+    
+            MidiNote.C_5,
+            MidiNote.C_SHARP_5,
+            MidiNote.D_FLAT_5,
+            MidiNote.D_5,
+            MidiNote.D_SHARP_5,
+            MidiNote.E_FLAT_5,
+            MidiNote.E_5,
+            MidiNote.F_5,
+            MidiNote.F_SHARP_5,
+            MidiNote.G_FLAT_5,
+            MidiNote.G_5,
+            MidiNote.G_SHARP_5,
+            MidiNote.A_FLAT_5,
+            MidiNote.A_5,
+            MidiNote.A_SHARP_5,
+            MidiNote.B_FLAT_5,
+            MidiNote.B_5,
+    
+            MidiNote.C_6,
+            MidiNote.C_SHARP_6,
+            MidiNote.D_FLAT_6,
+            MidiNote.D_6,
+            MidiNote.D_SHARP_6,
+            MidiNote.E_FLAT_6,
+            MidiNote.E_6,
+            MidiNote.F_6,
+            MidiNote.F_SHARP_6,
+            MidiNote.G_FLAT_6,
+            MidiNote.G_6,
+            MidiNote.G_SHARP_6,
+            MidiNote.A_FLAT_6,
+            MidiNote.A_6,
+            MidiNote.A_SHARP_6,
+            MidiNote.B_FLAT_6,
+            MidiNote.B_6,
+    
+            MidiNote.C_7,
+            MidiNote.C_SHARP_7,
+            MidiNote.D_FLAT_7,
+            MidiNote.D_7,
+            MidiNote.D_SHARP_7,
+            MidiNote.E_FLAT_7,
+            MidiNote.E_7,
+            MidiNote.F_7,
+            MidiNote.F_SHARP_7,
+            MidiNote.G_FLAT_7,
+            MidiNote.G_7,
+            MidiNote.G_SHARP_7,
+            MidiNote.A_FLAT_7,
+            MidiNote.A_7,
+            MidiNote.A_SHARP_7,
+            MidiNote.B_FLAT_7,
+            MidiNote.B_7,
+    
+            MidiNote.C_8
+        };
+    }
+
+    private void initNoteArray() {
+        noteIDArray = new int[]{
+            NamedNote.A_0,
+            NamedNote.A_SHARP_0,
+            NamedNote.B_FLAT_0,
+            NamedNote.B_0,
+
+            NamedNote.C_1,
+            NamedNote.C_SHARP_1,
+            NamedNote.D_FLAT_1,
+            NamedNote.D_1,
+            NamedNote.D_SHARP_1,
+            NamedNote.E_FLAT_1,
+            NamedNote.E_1,
+            NamedNote.F_1,
+            NamedNote.F_SHARP_1,
+            NamedNote.G_FLAT_1,
+            NamedNote.G_1,
+            NamedNote.G_SHARP_1,
+            NamedNote.A_FLAT_1,
+            NamedNote.A_1,
+            NamedNote.A_SHARP_1,
+            NamedNote.B_FLAT_1,
+            NamedNote.B_1,
+
+            NamedNote.C_2,
+            NamedNote.C_SHARP_2,
+            NamedNote.D_FLAT_2,
+            NamedNote.D_2,
+            NamedNote.D_SHARP_2,
+            NamedNote.E_FLAT_2,
+            NamedNote.E_2,
+            NamedNote.F_2,
+            NamedNote.F_SHARP_2,
+            NamedNote.G_FLAT_2,
+            NamedNote.G_2,
+            NamedNote.G_SHARP_2,
+            NamedNote.A_FLAT_2,
+            NamedNote.A_2,
+            NamedNote.A_SHARP_2,
+            NamedNote.B_FLAT_2,
+            NamedNote.B_2,
+
+            NamedNote.C_3,
+            NamedNote.C_SHARP_3,
+            NamedNote.D_FLAT_3,
+            NamedNote.D_3,
+            NamedNote.D_SHARP_3,
+            NamedNote.E_FLAT_3,
+            NamedNote.E_3,
+            NamedNote.F_3,
+            NamedNote.F_SHARP_3,
+            NamedNote.G_FLAT_3,
+            NamedNote.G_3,
+            NamedNote.G_SHARP_3,
+            NamedNote.A_FLAT_3,
+            NamedNote.A_3,
+            NamedNote.A_SHARP_3,
+            NamedNote.B_FLAT_3,
+            NamedNote.B_3,
+
+            NamedNote.C_4,
+            NamedNote.C_SHARP_4,
+            NamedNote.D_FLAT_4,
+            NamedNote.D_4,
+            NamedNote.D_SHARP_4,
+            NamedNote.E_FLAT_4,
+            NamedNote.E_4,
+            NamedNote.F_4,
+            NamedNote.F_SHARP_4,
+            NamedNote.G_FLAT_4,
+            NamedNote.G_4,
+            NamedNote.G_SHARP_4,
+            NamedNote.A_FLAT_4,
+            NamedNote.A_4,
+            NamedNote.A_SHARP_4,
+            NamedNote.B_FLAT_4,
+            NamedNote.B_4,
+
+            NamedNote.C_5,
+            NamedNote.C_SHARP_5,
+            NamedNote.D_FLAT_5,
+            NamedNote.D_5,
+            NamedNote.D_SHARP_5,
+            NamedNote.E_FLAT_5,
+            NamedNote.E_5,
+            NamedNote.F_5,
+            NamedNote.F_SHARP_5,
+            NamedNote.G_FLAT_5,
+            NamedNote.G_5,
+            NamedNote.G_SHARP_5,
+            NamedNote.A_FLAT_5,
+            NamedNote.A_5,
+            NamedNote.A_SHARP_5,
+            NamedNote.B_FLAT_5,
+            NamedNote.B_5,
+
+            NamedNote.C_6,
+            NamedNote.C_SHARP_6,
+            NamedNote.D_FLAT_6,
+            NamedNote.D_6,
+            NamedNote.D_SHARP_6,
+            NamedNote.E_FLAT_6,
+            NamedNote.E_6,
+            NamedNote.F_6,
+            NamedNote.F_SHARP_6,
+            NamedNote.G_FLAT_6,
+            NamedNote.G_6,
+            NamedNote.G_SHARP_6,
+            NamedNote.A_FLAT_6,
+            NamedNote.A_6,
+            NamedNote.A_SHARP_6,
+            NamedNote.B_FLAT_6,
+            NamedNote.B_6,
+
+            NamedNote.C_7,
+            NamedNote.C_SHARP_7,
+            NamedNote.D_FLAT_7,
+            NamedNote.D_7,
+            NamedNote.D_SHARP_7,
+            NamedNote.E_FLAT_7,
+            NamedNote.E_7,
+            NamedNote.F_7,
+            NamedNote.F_SHARP_7,
+            NamedNote.G_FLAT_7,
+            NamedNote.G_7,
+            NamedNote.G_SHARP_7,
+            NamedNote.A_FLAT_7,
+            NamedNote.A_7,
+            NamedNote.A_SHARP_7,
+            NamedNote.B_FLAT_7,
+            NamedNote.B_7,
+
+            NamedNote.C_8
+        };
+    }
+
+}
---
+++ src/main/java/note_coordinate/MidiNote.java
@@ -0,0 +1,175 @@
+package note_coordinate;
+
+public class MidiNote {
+
+    public static final int A_0 = 211;
+    public static final int A_SHARP_0 = 222;
+    public static final int B_FLAT_0 = 220;
+    public static final int B_0 = 231;
+
+    public static final int C_1 = 241;
+    public static final int C_SHARP_1 = 252;
+    public static final int D_FLAT_1 = 250;
+    public static final int D_1 = 261;
+    public static final int D_SHARP_1 = 272;
+    public static final int E_FLAT_1 = 271;
+    public static final int E_1 = 281;
+    public static final int F_1 = 291;
+    public static final int F_SHARP_1 = 302;
+    public static final int G_FLAT_1 = 301;
+    public static final int G_1 = 311;
+    public static final int G_SHARP_1 = 322;
+    public static final int A_FLAT_1 = 320;
+    public static final int A_1 = 331;
+    public static final int A_SHARP_1 = 342;
+    public static final int B_FLAT_1 = 340;
+    public static final int B_1 = 351;
+
+    public static final int C_2 = 361;
+    public static final int C_SHARP_2 = 372;
+    public static final int D_FLAT_2 = 370;
+    public static final int D_2 = 381;
+    public static final int D_SHARP_2 = 392;
+    public static final int E_FLAT_2 = 390;
+    public static final int E_2 = 401;
+    public static final int F_2 = 411;
+    public static final int F_SHARP_2 = 422;
+    public static final int G_FLAT_2 = 420;
+    public static final int G_2 = 431;
+    public static final int G_SHARP_2 = 442;
+    public static final int A_FLAT_2 = 440;
+    public static final int A_2 = 451;
+    public static final int A_SHARP_2 = 462;
+    public static final int B_FLAT_2 = 460;
+    public static final int B_2 = 471;
+
+    public static final int C_3 = 481;
+    public static final int C_SHARP_3 = 492;
+    public static final int D_FLAT_3 = 490;
+    public static final int D_3 = 501;
+    public static final int D_SHARP_3 = 512;
+    public static final int E_FLAT_3 = 510;
+    public static final int E_3 = 521;
+    public static final int F_3 = 531;
+    public static final int F_SHARP_3 = 542;
+    public static final int G_FLAT_3 = 540;
+    public static final int G_3 = 551;
+    public static final int G_SHARP_3 = 562;
+    public static final int A_FLAT_3 = 560;
+    public static final int A_3 = 571;
+    public static final int A_SHARP_3 = 582;
+    public static final int B_FLAT_3 = 580;
+    public static final int B_3 = 591;
+
+    public static final int C_4 = 601;
+    public static final int C_SHARP_4 = 612;
+    public static final int D_FLAT_4 = 610;
+    public static final int D_4 = 621;
+    public static final int D_SHARP_4 = 632;
+    public static final int E_FLAT_4 = 630;
+    public static final int E_4 = 641;
+    public static final int F_4 = 651;
+    public static final int F_SHARP_4 = 662;
+    public static final int G_FLAT_4 = 660;
+    public static final int G_4 = 671;
+    public static final int G_SHARP_4 = 682;
+    public static final int A_FLAT_4 = 680;
+    public static final int A_4 = 691;
+    public static final int A_SHARP_4 = 702;
+    public static final int B_FLAT_4 = 700;
+    public static final int B_4 = 711;
+
+    public static final int C_5 = 721;
+    public static final int C_SHARP_5 = 732;
+    public static final int D_FLAT_5 = 730;
+    public static final int D_5 = 741;
+    public static final int D_SHARP_5 = 752;
+    public static final int E_FLAT_5 = 750;
+    public static final int E_5 = 761;
+    public static final int F_5 = 771;
+    public static final int F_SHARP_5 = 782;
+    public static final int G_FLAT_5 = 780;
+    public static final int G_5 = 791;
+    public static final int G_SHARP_5 = 802;
+    public static final int A_FLAT_5 = 800;
+    public static final int A_5 = 811;
+    public static final int A_SHARP_5 = 822;
+    public static final int B_FLAT_5 = 820;
+    public static final int B_5 = 831;
+
+    public static final int C_6 = 841;
+    public static final int C_SHARP_6 = 852;
+    public static final int D_FLAT_6 = 850;
+    public static final int D_6 = 861;
+    public static final int D_SHARP_6 = 872;
+    public static final int E_FLAT_6 = 870;
+    public static final int E_6 = 881;
+    public static final int F_6 = 891;
+    public static final int F_SHARP_6 = 902;
+    public static final int G_FLAT_6 = 900;
+    public static final int G_6 = 911;
+    public static final int G_SHARP_6 = 922;
+    public static final int A_FLAT_6 = 920;
+    public static final int A_6 = 931;
+    public static final int A_SHARP_6 = 942;
+    public static final int B_FLAT_6 = 940;
+    public static final int B_6 = 951;
+
+    public static final int C_7 = 961;
+    public static final int C_SHARP_7 = 972;
+    public static final int D_FLAT_7 = 970;
+    public static final int D_7 = 981;
+    public static final int D_SHARP_7 = 992;
+    public static final int E_FLAT_7 = 990;
+    public static final int E_7 = 1001;
+    public static final int F_7 = 1011;
+    public static final int F_SHARP_7 = 1022;
+    public static final int G_FLAT_7 = 1020;
+    public static final int G_7 = 1031;
+    public static final int G_SHARP_7 = 1042;
+    public static final int A_FLAT_7 = 1040;
+    public static final int A_7 = 1051;
+    public static final int A_SHARP_7 = 1062;
+    public static final int B_FLAT_7 = 1060;
+    public static final int B_7 = 1071;
+
+    public static final int C_8 = 1081;
+
+    public static final int FLAT = 0;
+    public static final int NO_ACCIDENTAL = 1;
+    public static final int SHARP = 2;
+    private int id;
+    private int midiValue;
+    private int requestedAccidental;
+    private MidiAndNoteBimap mapper;
+
+    public MidiNote(int midiValue, int requestedAccidental) {
+        this.midiValue = midiValue;
+        this.requestedAccidental = requestedAccidental;
+        this.id = midiValue * 10 + requestedAccidental;
+    }
+
+    public MidiNote(int midiID) {
+        this.id = midiID;
+        this.midiValue = midiID / 10;               // drops right digit
+        this.requestedAccidental = midiID % 10;     // isolates right digit
+    }
+
+    public NamedNote toNamedNote() {
+        mapper = new MidiAndNoteBimap();
+        int noteID = mapper.getNoteIDWithMidiID(this.id);
+        return new NamedNote(noteID);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getMidiValue() {
+        return midiValue;
+    }
+
+    public int getRequestedAccidental() {
+        return requestedAccidental;
+    }
+}
