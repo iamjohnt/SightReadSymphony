3843c7039d3b6353835e28efd031d111d0a6063c
johnt <tranjohn123@gmail.com>
2021-07-29 11:39:17 -0400

    added functionality to draw any key signature set of symbols

--- src/main/java/draw/Draw.java
+++ src/main/java/draw/Draw.java
@@ -3,6 +3,7 @@ package draw;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import logic.Config;
+import notecontext.KeySignature;
 import notecontext.NamedNote;
 import notecontext.NoteContext;
 
@@ -15,6 +16,21 @@ public class Draw {
     private NoteContext noteContext;
     private Config config;
     private double lineHeight;
+    private static final String G_MAJOR_E_MINOR_IMAGE_PATH = "src/main/resources/images/g_major_e_minor.png";
+    private static final String D_MAJOR_B_MINOR_IMAGE_PATH = "src/main/resources/images/d_major_b_minor.png";
+    private static final String A_MAJOR_F_SHARP_MINOR = "src/main/resources/images/a_major_f_sharp_minor.png";
+    private static final String E_MAJOR_C_SHARP_MINOR = "src/main/resources/images/e_major_c_sharp_minor.png";
+    private static final String B_MAJOR_G_SHARP_MINOR = "src/main/resources/images/b_major_g_sharp_minor.png";
+    private static final String F_SHARP_MAJOR_D_SHARP_MINOR = "src/main/resources/images/f_sharp_major_d_sharp_minor.png";
+    private static final String F_MAJOR_D_MINOR = "src/main/resources/images/f_major_d_minor.png";
+    private static final String B_FLAT_MAJOR_G_MINOR = "src/main/resources/images/b_flat_major_g_minor.png";
+    private static final String E_FLAT_MAJOR_C_MINOR = "src/main/resources/images/e_flat_major_c_minor.png";
+    private static final String A_FLAT_MAJOR_F_MINOR = "src/main/resources/images/a_flat_major_f_minor.png";
+    private static final String D_FLAT_MAJOR_B_FLAT_MINOR = "src/main/resources/images/d_flat_major_b_flat_minor.png";
+    private static final String G_FLAT_MAJOR_E_FLAT_MINOR = "src/main/resources/images/g_flat_major_e_flat_minor.png";
+    private static final String C_SHARP_MAJOR = "src/main/resources/images/c_sharp_major.png";
+    private static final String C_FLAT_MINOR = "src/main/resources/images/c_flat_minor.png";
+    private static final String C_FLAT_MAJOR = "src/main/resources/images/c_flat_major.png";
 
     public Draw(GraphicsContext gc, Config config) {
         this.graphicsContext = gc;
@@ -31,6 +47,13 @@ public class Draw {
         double bassX1 = config.getBassClefX();
         double bassX2 = config.getBassClefX() + config.getTrebleClefWidth();
         drawBassClef(bassX1, bassX2);
+
+        String keySigImagePath = determineKeySigPath();
+        if (noteContext.getKeySigAccidental() == KeySignature.SHRP) {
+            drawSharpKeySigs(keySigImagePath);
+        } else if (noteContext.getKeySigAccidental() == KeySignature.FLAT){
+            drawFlatKeySigs(keySigImagePath);
+        }
     }
 
     private void drawTrebleClef(double x1, double x2) {
@@ -72,6 +95,66 @@ public class Draw {
         }
         return new Image(urlString, h, h,true,true);
     }
+
+    private void drawSharpKeySigs(String imagePath) {
+        Image keySigSymbols = createImage(imagePath, lineHeight * 6.25);
+        double trebleX = config.getTrebleClefX() + (lineHeight * 4);
+        double trebleY = config.getTrebleClefY() - (lineHeight * 1.75);
+        double bassX = config.getBassClefX() + (lineHeight * 4);
+        double bassY = config.getBassClefY() - (lineHeight);
+        graphicsContext.drawImage(keySigSymbols, trebleX , trebleY);
+        graphicsContext.drawImage(keySigSymbols, bassX , bassY);
+    }
+
+
+    private void drawFlatKeySigs(String imagePath) {
+        Image keySigSymbols = createImage(imagePath, lineHeight * 6.5);
+        double trebleX = config.getTrebleClefX() + (lineHeight * 4);
+        double trebleY = config.getTrebleClefY() - (lineHeight * 1.20);
+        graphicsContext.drawImage(keySigSymbols, trebleX , trebleY);
+
+        double bassX = config.getBassClefX() + (lineHeight * 4);
+        double bassY = config.getBassClefY() - (lineHeight * 0.2);
+        graphicsContext.drawImage(keySigSymbols, bassX , bassY);
+    }
+
+    private String determineKeySigPath() {
+        String rtn = null;
+        int keySigID = noteContext.getKeySigID();
+        switch (keySigID) {
+            case KeySignature.C_MAJOR_ID: rtn = null; break;
+            case KeySignature.A_MINOR_ID: rtn = null; break;
+            case KeySignature.G_MAJOR_ID: rtn = G_MAJOR_E_MINOR_IMAGE_PATH; break;
+            case KeySignature.D_MAJOR_ID: rtn = D_MAJOR_B_MINOR_IMAGE_PATH; break;
+            case KeySignature.A_MAJOR_ID: rtn = A_MAJOR_F_SHARP_MINOR; break;
+            case KeySignature.E_MAJOR_ID: rtn = E_MAJOR_C_SHARP_MINOR; break;
+            case KeySignature.B_MAJOR_ID: rtn = B_MAJOR_G_SHARP_MINOR; break;
+            case KeySignature.F_SHARP_MAJOR_ID: rtn = F_SHARP_MAJOR_D_SHARP_MINOR; break;
+            case KeySignature.C_SHARP_MAJOR_ID: rtn = C_SHARP_MAJOR; break;
+            case KeySignature.E_MINOR_ID: rtn = G_MAJOR_E_MINOR_IMAGE_PATH; break;
+            case KeySignature.B_MINOR_ID: rtn = D_MAJOR_B_MINOR_IMAGE_PATH; break;
+            case KeySignature.F_SHARP_MINOR_ID: rtn = A_MAJOR_F_SHARP_MINOR; break;
+            case KeySignature.C_SHARP_MINOR_ID: rtn = E_MAJOR_C_SHARP_MINOR; break;
+            case KeySignature.G_SHARP_MINOR_ID: rtn = B_MAJOR_G_SHARP_MINOR; break;
+            case KeySignature.D_SHARP_MINOR_ID: rtn = F_SHARP_MAJOR_D_SHARP_MINOR; break;
+            case KeySignature.F_MAJOR_ID: rtn = F_MAJOR_D_MINOR; break;
+            case KeySignature.B_FLAT_MAJOR_ID: rtn = B_FLAT_MAJOR_G_MINOR; break;
+            case KeySignature.E_FLAT_MAJOR_ID: rtn = E_FLAT_MAJOR_C_MINOR; break;
+            case KeySignature.A_FLAT_MAJOR_ID: rtn = A_FLAT_MAJOR_F_MINOR; break;
+            case KeySignature.D_FLAT_MAJOR_ID: rtn = D_FLAT_MAJOR_B_FLAT_MINOR; break;
+            case KeySignature.G_FLAT_MAJOR_ID: rtn = G_FLAT_MAJOR_E_FLAT_MINOR; break;
+            case KeySignature.C_FLAT_MAJOR_ID: rtn = C_FLAT_MAJOR; break;
+            case KeySignature.D_MINOR_ID: rtn = F_MAJOR_D_MINOR; break;
+            case KeySignature.G_MINOR_ID: rtn = B_FLAT_MAJOR_G_MINOR; break;
+            case KeySignature.C_MINOR_ID: rtn = E_FLAT_MAJOR_C_MINOR; break;
+            case KeySignature.F_MINOR_ID: rtn = A_FLAT_MAJOR_F_MINOR; break;
+            case KeySignature.B_FLAT_MINOR_ID: rtn = D_FLAT_MAJOR_B_FLAT_MINOR; break;
+            case KeySignature.E_FLAT_MINOR_ID: rtn = G_FLAT_MAJOR_E_FLAT_MINOR; break;
+            default: rtn = "switch case default";
+        }
+        return rtn;
+    }
+
 }
 
 
