37de72ac64a32c6af05c34779b19c533359b50cb
johnt <tranjohn123@gmail.com>
2021-08-03 14:33:40 -0400

    created ability to animate all music objects in a queue at atime

--- src/main/java/game/MusicObjectAnimator.java
+++ src/main/java/game/MusicObjectAnimator.java
@@ -1,10 +1,12 @@
 package game;
 
 import javafx.animation.TranslateTransition;
+import javafx.scene.Node;
 import javafx.scene.image.ImageView;
 import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 
+import java.util.LinkedList;
 import java.util.Queue;
 
 public class MusicObjectAnimator {
@@ -12,29 +14,30 @@ public class MusicObjectAnimator {
     private boolean isAnimating = false;
 
     public void moveAllMusicObjectsLeft(Queue<MusicObject> musicObjects, double xDistance) {
-        isAnimating = true;
-        while (!musicObjects.isEmpty()) {
-            MusicObject curr = musicObjects.remove();
-            if (musicObjects.isEmpty()) {
-                moveMusicObject(curr, xDistance, true);
-            } else {
-                moveMusicObject(curr, xDistance, false);
+        System.out.println(isAnimating);
+        if (isAnimating == false) {
+            isAnimating = true;
+            LinkedList<MusicObject> linkedList = (LinkedList) musicObjects;
+            int lastOne = linkedList.size() - 1;
+            for (int i = 0; i < linkedList.size(); i++) {
+                boolean isLastOne = i == lastOne;
+                moveMusicObject(linkedList.get(i), xDistance, isLastOne);
             }
         }
     }
 
     private void moveMusicObject(MusicObject musicObject, double xDistance, boolean isLastMusicObject) {
-        ImageView[] notes = musicObject.getNotesViews();
-        ImageView[] accidentals = musicObject.getNotesViews();
-        Rectangle[] ledgers = musicObject.getLedgerRectangles();
+        Node[] notes = musicObject.getNotesViews();
+        Node[] accidentals = musicObject.getAccidentalViews();
+        Node[] ledgers = musicObject.getLedgerRectangles();
+        Node[] noteInfos = musicObject.getDescriptionLabels();
 
-        isAnimating = true;
         for (int i = 0; i < notes.length; i++) {
             TranslateTransition trans = new TranslateTransition();
             trans.setDuration(Duration.millis(50));
             trans.setByX(xDistance);
-            trans.setCycleCount(4);
-            trans.setAutoReverse(true);
+            trans.setCycleCount(1);
+            trans.setAutoReverse(false);
             trans.setNode(notes[i]);
             trans.play();
         }
@@ -42,9 +45,18 @@ public class MusicObjectAnimator {
             TranslateTransition trans = new TranslateTransition();
             trans.setDuration(Duration.millis(50));
             trans.setByX(xDistance);
-            trans.setCycleCount(4);
-            trans.setAutoReverse(true);
+            trans.setCycleCount(1);
+            trans.setAutoReverse(false);
             trans.setNode(accidentals[i]);
+            trans.play();
+        }
+        for (int i = 0; i < noteInfos.length; i++) {
+            TranslateTransition trans = new TranslateTransition();
+            trans.setDuration(Duration.millis(50));
+            trans.setByX(xDistance);
+            trans.setCycleCount(1);
+            trans.setAutoReverse(false);
+            trans.setNode(noteInfos[i]);
             trans.play();
         }
         for (int i = 0; i < ledgers.length; i++) {
@@ -52,10 +64,11 @@ public class MusicObjectAnimator {
             trans.setDuration(Duration.millis(50));
             trans.setByX(xDistance);
             trans.setCycleCount(4);
-            trans.setAutoReverse(true);
+            trans.setAutoReverse(false);
             trans.setNode(ledgers[i]);
             if (isLastMusicObject) {
                 trans.setOnFinished(event -> isAnimating = false);
+                System.out.println("set is false HERE");
             }
             trans.play();
         }
